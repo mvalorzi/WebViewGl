e6646ea16a3709be6b2ef1ec6e440ce89ef0f479
Martín Valorzi <91589102+mvalorzi@users.noreply.github.com>
2021-12-12 20:41:21 -0300

    Add all files

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    private var webView: WKWebView!
+    private var urlString = "https://cdn-webgl.fluidconfigure.com/webgl-viewer/examples/demos/model-loading.html"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let webViewPrefs = WKWebpagePreferences()
+        webViewPrefs.allowsContentJavaScript = true
+        let webViewConfiguration = WKWebViewConfiguration()
+        webViewConfiguration.defaultWebpagePreferences = webViewPrefs
+        webView = WKWebView(frame: view.frame, configuration: webViewConfiguration)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(webView)
+        load(url: urlString)
     }
-
-
+    private func load(url: String){
+        webView.load(URLRequest(url:URL(string: url)!))
+    }
+    func webViewHandler(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        guard let serverTrust = challenge.protectionSpace.serverTrust else {
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+        let exceptions = SecTrustCopyExceptions(serverTrust)
+        SecTrustSetExceptions(serverTrust, exceptions)
+        completionHandler(.useCredential, URLCredential(trust: serverTrust));
+    }
 }
 
